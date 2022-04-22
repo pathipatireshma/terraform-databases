053500ac5adc1255bcda4544aa5fd1c9a42ec667
pathipatireshma <pathipati.reshma@gmail.com>
2022-04-22 13:12:19 +0530

    Updating terraform-databases

---
+++ main.tf
@@ -0,0 +1,30 @@
+module "mongodb" {
+  source                = "./mongodb"
+  ENV                   = var.ENV
+  MONGODB_INSTANCE_TYPE = var.MONGODB_INSTANCE_TYPE
+
+}
+
+module "rabbitmq" {
+  source                 = "./rabbitmq"
+  ENV                    = var.ENV
+  RABBITMQ_INSTANCE_TYPE = var.RABBITMQ_INSTANCE_TYPE
+}
+
+
+
+module "mysql" {
+  source = "./mysql"
+  ENV    = var.ENV
+}
+
+module "redis" {
+  source = "./redis"
+  ENV    = var.ENV
+}
+
+
+
+output "redis" {
+  value = module.redis.redis
+}
